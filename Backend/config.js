98ffd5c8900bc90f58efafec5d3742ffab0895bb
vasusumeet <vasuseth1305@gmail.com>
2025-06-12 03:51:00 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 5555;
+export const PORT = process.env.PORT;
 export const mongoDBURL = process.env.MONGODB_URL;
 export const jwtSecret= process.env.JWT_SECRET;
 
